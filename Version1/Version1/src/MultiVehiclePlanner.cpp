d88e9f2334a2b55efb354d3d55bb810dacbd78c9
Vahe Eminyan <vahe.eminyan1999@gmail.com>
2021-10-29 22:58:48 +0200

    MultiVehiclePlanner -> UpdatePlanner changed. More effichent.

--- Version1/Version1/src/MultiVehiclePlanner.cpp
+++ Version1/Version1/src/MultiVehiclePlanner.cpp
@@ -13,16 +13,17 @@ void MultiVehiclePlanner::UpdatePlanner(Time step_size, Time /*planning_time*/) 
       return;
 
     VehicleState currentState = planner->GetCurrentVehicleState();
-
+    double dist = 0;
     // Try to detect collisions with obstacles
     bool collisiondetected = false;
     Pos curStatePos = currentState.GetPosition();
-    Vel velo = currentState.GetVelocity();
+    //Vel velo = currentState.GetVelocity();
     for (auto obstacle : this->GetObstacles()) {
       // Avoid Collisions with objects.
       Pos obstacle_position = obstacle.second.GetPosition();
       double distance = std::sqrt(std::pow(obstacle_position.x() - curStatePos.x(), 2) + std::pow(obstacle_position.y() - curStatePos.y(), 2));
-      if (distance < 0.7) {
+      dist = distance;
+      if (distance < 0.6) {
         collisiondetected = true;
       }else{
         collisiondetected = false;
@@ -30,19 +31,27 @@ void MultiVehiclePlanner::UpdatePlanner(Time step_size, Time /*planning_time*/) 
     }
 
     // handle the action of the car (vehicleStates)
-    if (collisiondetected) {
+    if (collisiondetected ) {
       // handle collision with obstacle (stop the vehicle until the obstacle moved away)
       std::cout << "Collision " << int(id) << std::endl;
-      currentState.SetTime(currentState.GetTime() + step_size);
-      currentState.SetVelocity({0.0, 0.0});
+      planner->GetCurrentVehicleState().SetTime(planner->GetCurrentVehicleState().GetTime() + step_size);
+      planner->GetCurrentVehicleState().SetVelocity({0.0, 0.0});
+      std::cout << "Kollision erkannt" << planner->GetCurrentVehicleState().GetPosition() << std::endl;
+      planner->Update(0);
       planner->UpdateTimer(step_size);
-    } else {
+    }
+    else {
       // everything fine! Just keep going.
       std::cout << "Update Planner " << int(id) << std::endl;
+      std::cout << "------------------------------------------------"<< std::endl;
+      std::cout << "Vor dem Update" << planner->GetCurrentVehicleState().GetPosition() << std::endl;
       planner->Update(step_size);
+      std::cout << "Nach dem Update" << planner->GetCurrentVehicleState().GetPosition() << std::endl;
+      std::cout << "------------------------------------------------"<< std::endl;
+      
     }
-
-    this->AddVehicleState(id, currentState, "MultiVehiclePlanner");
+    std::cout << "Am Ende" << planner->GetCurrentVehicleState().GetPosition() << std::endl;
+    this->AddVehicleState(id, planner->GetCurrentVehicleState(), "MultiVehiclePlanner");
 
     // If planner finished stop planning
     if (planner->IsTargetPositionReached()) {
